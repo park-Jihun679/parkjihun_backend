cf247f85f5f3f128f70885fc38c15791500cf102
박지훈 <wlgns679@gmail.com>
2026-01-11 02:17:19 +0900

    fix: 한도 초기화 업데이트 쿼리 수정

--- src/main/java/org/wireBarley/account/repository/AccountRepository.java
+++ src/main/java/org/wireBarley/account/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package org.wireBarley.account.repository;
 import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import java.util.UUID;
+import org.antlr.v4.runtime.atn.SemanticContext.AND;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
@@ -14,15 +15,14 @@ public interface AccountRepository extends JpaRepository<AccountEntity, UUID> {
     Optional<AccountEntity> findByAccountNo(String accountNo);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("""
-            SELECT a
-            FROM AccountEntity a
-            WHERE a.accountNo = :accountNo
-              AND a.isDeleted = false
-        """)
+    @Query("SELECT a FROM AccountEntity a WHERE a.accountNo = :accountNo AND a.isDeleted = false")
     Optional<AccountEntity> findByAccountNoForUpdate(String accountNo);
 
     @Modifying
-    @Query("UPDATE AccountEntity a SET a.dailyTotalWithdrawal = 0, a.dailyTotalTransfer = 0")
+    @Query("""
+        UPDATE AccountEntity a
+        SET a.dailyTotalWithdrawal = 0, a.dailyTotalTransfer = 0, a.updatedDate = CURRENT_TIMESTAMP
+        WHERE a.isDeleted = false
+        """)
     int resetDailyLimits();
 }
